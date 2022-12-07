4011c4dc280b8d8ea99d518abe0cf408a5913402
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2022-12-08 00:05:32 +0200

    fin

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,70 +1,54 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import Form from 'components/Form';
 import Filter from 'components/Filter';
 import Contacts from 'components/Contacts';
 
-export class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-        // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-        // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-        // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-        // { id: 'id-5', name: 'Edem Cldfmts', number: '645-17-79' },
-        // { id: 'id-6', name: 'Alec Mjduels', number: '645-17-79' },
-        // { id: 'id-7', name: 'Karl Fridr', number: '645-17-79' },
-        // { id: 'id-8', name: 'Joiur Masuro', number: '645-17-79' },
-      ],
-      filter: '',
-    };
-  }
+export const App = () => {
+  const [contacts, setContacts] = useState(
+    () => JSON.parse(window.localStorage.getItem('phoneBook')) ?? ''
+    // [
+    //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    //     { id: 'id-5', name: 'Edem Cldfmts', number: '645-17-79' },
+    //     { id: 'id-6', name: 'Alec Mjduels', number: '645-17-79' },
+    //     { id: 'id-7', name: 'Karl Fridr', number: '645-17-79' },
+    //     { id: 'id-8', name: 'Joiur Masuro', number: '645-17-79' },
+    //   ]
+  );
+  const [filter, setFilter] = useState('');
 
-  componentDidMount() {
-    const dataParsed = JSON.parse(localStorage.getItem('phoneBook'));
-    if (!dataParsed) return;
-    this.setState({ contacts: dataParsed });
-  }
+  useEffect(() => {
+    window.localStorage.setItem('phoneBook', JSON.stringify(contacts));
+  }, [contacts]);
 
-  componentDidUpdate() {
-    localStorage.setItem('phoneBook', JSON.stringify(this.state.contacts));
-  }
-
-  formSubmitHandler = data => {
-    this.state.contacts.map(el => {
+  const formSubmitHandler = data => {
+    contacts.map(el => {
       if (el.name.toLowerCase() === data.name.toLowerCase()) {
         alert(`${data.name} is already in contacts.`);
       }
       return el.name;
     });
-    this.setState({ contacts: [...this.state.contacts, data] });
+    setContacts([...contacts, data]);
   };
 
-  handleChangeFilter = data => {
-    this.setState({ filter: data });
+  const handleChangeFilter = data => {
+    setFilter(data);
   };
 
-  handleClickDelete = data => {
+  const handleClickDelete = data => {
     console.log(data);
-    this.setState({
-      contacts: this.state.contacts.filter(el => el.id !== data),
-    });
+    setContacts(contacts.filter(el => el.id !== data));
   };
 
-  render() {
-    return (
-      <div>
-        <h2>Phonebook</h2>
-        <Form onSubmit={this.formSubmitHandler} />
-        <h2>Contacts</h2>
-        <Filter onChange={this.handleChangeFilter} />
-        <Contacts
-          onDelete={this.handleClickDelete}
-          data={this.state.contacts}
-          filter={this.state.filter}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Form onSubmit={formSubmitHandler} />
+      <h2>Contacts</h2>
+      <Filter onChange={handleChangeFilter} />
+      <Contacts onDelete={handleClickDelete} data={contacts} filter={filter} />
+    </div>
+  );
+};
